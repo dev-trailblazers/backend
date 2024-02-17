d60a5ad3b433aeaec8f070c3a64f3e4dfaa82d57
정찬영 <jcy0342@gmail.com>
2024-02-17 21:20:14 +0900

    refactor: Response entity status 코드 수정

--- src/main/java/com/growth/community/controller/CommentController.java
+++ src/main/java/com/growth/community/controller/CommentController.java
@@ -1,16 +1,16 @@
 package com.growth.community.controller;
 
-import com.growth.community.domain.comment.dto.CommentDto;
 import com.growth.community.domain.comment.dto.RequestCommentDto;
 import com.growth.community.domain.user.dto.Principal;
 import com.growth.community.service.CommentService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 @RequiredArgsConstructor
 @RequestMapping("/comments")
@@ -22,7 +22,7 @@ public class CommentController {
     public ResponseEntity<Void> postComment(@RequestBody @Valid RequestCommentDto dto,
                                             @AuthenticationPrincipal Principal principal){
         commentService.createComment(dto, principal.id());
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+        return ResponseEntity.created(URI.create("/articles/id/" + dto.articleId())).build();
     }
 
     @PutMapping("/id/{commentId}")
@@ -30,14 +30,14 @@ public class CommentController {
                                            @RequestBody @NotBlank String content,
                                            @AuthenticationPrincipal Principal principal){
         commentService.updateComment(commentId, content, principal.id());
-        return ResponseEntity.status(HttpStatus.OK).build();
+        return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/id/{commentId}")
     public ResponseEntity<Void> deleteComment(@PathVariable Long commentId,
                                               @AuthenticationPrincipal Principal principal){
         commentService.deleteComment(commentId, principal.id());
-        return ResponseEntity.status(HttpStatus.OK).build();
+        return ResponseEntity.ok().build();
     }
 
 
