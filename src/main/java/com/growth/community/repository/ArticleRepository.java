6892d3674525a34431afcf0798396225b6c2756b
정찬영 <jcy0342@gmail.com>
2024-03-01 23:08:52 +0900

    feat: 게시글 목록 조회 시 게시글에 해당하는 댓글 수 추가

--- src/main/java/com/growth/community/repository/ArticleRepository.java
+++ src/main/java/com/growth/community/repository/ArticleRepository.java
@@ -12,17 +12,16 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+    @Query("SELECT COUNT(a) FROM Article a WHERE LOWER(a.title) LIKE LOWER(CONCAT('%', :keyword, '%')) OR LOWER(a.hashtags) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    Long countByKeyword(@Param("keyword") String keyword);
     @Query("SELECT a FROM Article a WHERE LOWER(a.title) LIKE LOWER(CONCAT('%', :keyword, '%')) OR LOWER(a.hashtags) LIKE LOWER(CONCAT('%', :keyword, '%'))")
     List<Article> findAllByKeyword(@Param("keyword") String keyword, Pageable pageable);
 
-    @Query("SELECT COUNT(a) FROM Article a WHERE LOWER(a.title) LIKE LOWER(CONCAT('%', :keyword, '%')) OR LOWER(a.hashtags) LIKE LOWER(CONCAT('%', :keyword, '%'))")
-    Long countByKeyword(@Param("keyword") String keyword);
-
-    Optional<Article> findByIdAndUserAccount_Id(Long articleId, Long userId);
-
     Long countByUserAccount_Id(Long userId);
     List<Article> findAllByUserAccount_Id(Long userId, Pageable pageable);
+    Optional<Article> findByIdAndUserAccount_Id(Long articleId, Long userId);
 
+<<<<<<< develop
 <<<<<<< develop
     @Query("SELECT COUNT (a) FROM Article a JOIN FETCH a.comments c JOIN c.userAccount u WHERE u.id = :userId")
     Long countByCommentAndUserId(Long userId);
@@ -31,5 +30,10 @@ public interface ArticleRepository extends JpaRepository<Article, Long> {
     Long countByCommentAndUserId(@Param("userId") Long userId);
 >>>>>>> feat: 내가 쓴 게시글, 댓글 단 게시글 조회 구현
     @Query("SELECT DISTINCT a FROM Article a JOIN FETCH a.comments c JOIN c.userAccount u WHERE u.id = :userId")
+=======
+    @Query("SELECT COUNT(DISTINCT a) FROM Article a JOIN a.comments c WHERE c.userAccount.id = :userId")
+    Long countByCommentAndUserId(@Param("userId") Long userId);
+    @Query("SELECT DISTINCT a FROM Article a JOIN FETCH a.comments c WHERE a.userAccount.id = :userId")
+>>>>>>> feat: 게시글 목록 조회 시 게시글에 해당하는 댓글 수 추가
     List<Article> findAllByCommentAndUserId(Long userId, Pageable pageable);
 }
