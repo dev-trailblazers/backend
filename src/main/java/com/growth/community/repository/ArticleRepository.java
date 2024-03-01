08c8e8676da0663e9b6b06c85097e699f438b5b2
정찬영 <jcy0342@gmail.com>
2024-03-01 23:21:08 +0900

    refactor: git 충돌 부분 삭제

--- src/main/java/com/growth/community/repository/ArticleRepository.java
+++ src/main/java/com/growth/community/repository/ArticleRepository.java
@@ -21,27 +21,6 @@ public interface ArticleRepository extends JpaRepository<Article, Long> {
     List<Article> findAllByUserAccount_Id(Long userId, Pageable pageable);
     Optional<Article> findByIdAndUserAccount_Id(Long articleId, Long userId);
 
-<<<<<<< develop
-<<<<<<< develop
-<<<<<<< develop
-<<<<<<< develop
-=======
-<<<<<<< b717eca99bd243f2fa8e8baebaac66d888736781
-=======
-<<<<<<< develop
-<<<<<<< develop
->>>>>>> feat: 게시글 목록 조회 시 게시글에 해당하는 댓글 수 추가
->>>>>>> feat: 게시글 목록 조회 시 게시글에 해당하는 댓글 수 추가
-    @Query("SELECT COUNT (a) FROM Article a JOIN FETCH a.comments c JOIN c.userAccount u WHERE u.id = :userId")
-    Long countByCommentAndUserId(Long userId);
-=======
-    @Query("SELECT COUNT(DISTINCT a) FROM Article a JOIN a.comments c JOIN c.userAccount u WHERE u.id = :userId")
-    Long countByCommentAndUserId(@Param("userId") Long userId);
->>>>>>> feat: 내가 쓴 게시글, 댓글 단 게시글 조회 구현
-    @Query("SELECT DISTINCT a FROM Article a JOIN FETCH a.comments c JOIN c.userAccount u WHERE u.id = :userId")
-=======
-=======
->>>>>>> refactor: git 충돌 부분 삭제
     @Query("SELECT COUNT(DISTINCT a) FROM Article a JOIN a.comments c WHERE c.userAccount.id = :userId")
     Long countByCommentAndUserId(@Param("userId") Long userId);
     @Query("SELECT DISTINCT a FROM Article a JOIN FETCH a.comments c WHERE a.userAccount.id = :userId")
